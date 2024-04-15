b40e3923fca2eb332b9887b337885ec36649a575
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-04-16 00:24:57 +0800

    add some css style and description

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-zinc-100 p-4 text-zinc-800 dark:text-white"
+  >
+    <div class="max-w-2xl mx-auto text-center">
+      <img
+        class="w-32 h-32 rounded-full mx-auto mb-4"
+        src="https://placehold.co/128x128"
+        alt="Profile image"
+      />
+      <h1 class="text-4xl font-bold mb-2">CREATIVE PORTFOLIO</h1>
+      <p class="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac
+        nulla consequat aliquet id quis turpis. Etiam nec elit tristique,
+        fermentum nunc vel, gravida nunc. Cras aliquet, nunc sit amet
+        scelerisque ullamcorper, leo dolor tempus magna, vitae cursus nisl dui
+        at ipsum. Donec et sapien pellentesque, fermentum felis vitae, ultricies
+        eros.
+      </p>
+      <div class="mb-8 px-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div class="bg-white bg-orange-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-lg font-bold mb-2">Hobbies</h3>
+            <p class="text-zinc-600">
+              I love watching youtube videos and listening to music. During my
+              free time, I like to scroll through social media and playing
+              mboile games with my friends.
+            </p>
+          </div>
+          <div class="bg-white bg-orange-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-lg font-bold mb-2">Interest</h3>
+            <p class="text-zinc-600">Cooking</p>
+            <p class="text-zinc-600">Photography</p>
+            <p class="text-zinc-600">Watching k-drama</p>
+            <p class="text-zinc-600">Shopping</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
